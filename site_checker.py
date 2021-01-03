c1a0e39423e749739f676477c688383f01d09894
Makeeyaf <makeeyaf@gmail.com>
2021-01-03 23:29:11 +0900

    클래스와 최상위 실행 스코프 분리

--- site_checker.py
+++ site_checker.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import enum
 import datetime
@@ -151,35 +150,3 @@ class SiteChecker:
                 )
 
         self.writeOption(self.path)
-
-
-parser = argparse.ArgumentParser(description="Check sites text.")
-parser.add_argument("config", type=str, nargs=1, help="Path to config json file.")
-parser.add_argument(
-    "-a", dest="apiKey", type=str, nargs=1, required=True, help="Pushbullet API key."
-)
-parser.add_argument(
-    "-m", dest="maxFailCount", type=int, nargs=1, help="Max fail count."
-)
-parser.add_argument(
-    "-u", dest="updateCycle", type=int, nargs=1, help="Update cycle in second"
-)
-parser.add_argument("-v", dest="isVerbose", action="store_true", help="Verbose mode.")
-parser.add_argument(
-    "-q",
-    dest="isQuiet",
-    action="store_true",
-    help="Quiet mode. Does not call pushbullet",
-)
-
-
-args = parser.parse_args()
-k = SiteChecker(
-    args.config[0],
-    args.apiKey[0],
-    args.isQuiet,
-    args.isVerbose,
-    args.maxFailCount,
-    args.updateCycle,
-)
-k.check()
